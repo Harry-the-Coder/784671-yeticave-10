f77804541f13c7f6decb608239419cd594688e20
Harry-the-Coder <denbukharev@gmail.com>
2019-08-10 14:12:20 +0400

    правки

--- index.php
+++ index.php
@@ -28,9 +28,9 @@ $user_name = 'Denis';
 
         <nav class="user-menu">
 
-        <?php if ($is_auth == 1): ?>
+        <?php if ($is_auth === 1): ?>
         <div class="user-menu__logged">
-            <p><?php print($user_name); ?></p>
+            <p><?= $user_name ?></p>
             <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
             <a class="user-menu__logout" href="#">Выход</a>
         </div>
