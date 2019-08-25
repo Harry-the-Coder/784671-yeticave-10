55cd4bb3de0f367fe29d16bbd664eaf9cf079776
Harry-the-Coder <denbukharev@gmail.com>
2019-08-25 22:07:03 +0400

    sql3

--- schema.sql
+++ schema.sql
@@ -5,40 +5,40 @@ DEFAULT COLLATE utf8_general_ci;
 USE Yeti;
 
 CREATE table categories (
-  id INT AUTO_INCREMENT PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name CHAR NOT NULL,
   symbol_code CHAR NOT NULL
 );
 
 CREATE table users (
-  id INT AUTO_INCREMENT PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   register_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   email CHAR NOT NULL UNIQUE,
   name CHAR NOT NULL,
   password CHAR NOT NULL UNIQUE,
   contacts CHAR UNIQUE,
-  lot_id INT NOT NULL,
-  rate_id INT NOT NULL
+  lot_id INT,
+  rate_id INT
 );
 
 CREATE table rates (
-  id INT AUTO_INCREMENT PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   register_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   description CHAR,
   image CHAR UNIQUE,
   start_price INT,
   finishing_date DATETIME,
   step INT,
-  user_id INT NOT NULL,
-  winner_id INT NOT NULL,
-  category_id INT NOT NULL
+  user_id INT,
+  winner_id INT,
+  category_id INT
 );
 
 CREATE table lots (
-  id INT AUTO_INCREMENT PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   register_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   price INT NOT NULL,
-  user_id INT NOT NULL,
-  lot_id INT NOT NULL
+  user_id INT,
+  lot_id INT
 );
 
